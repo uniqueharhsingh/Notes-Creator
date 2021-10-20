6ac73196fb7a0d116c0608654d1102599c509c5b
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2021-10-20 02:46:09 -0700

    update::

--- project.js
+++ project.js
@@ -1,1 +1,29 @@
 console.log("welcome to self notes")
+
+let Addbtn=document.getElementById("Addbtn");
+Addbtn.addEventListener("click",function(e){
+
+    let Addtxt=document.getElementById("Addtxt")
+    let notes=localStorage.getItem("notes");
+    if (notes==null){
+        notesobj=[];
+    }
+    else{
+       notesobj=JSON.parse(notes)
+
+    }
+
+
+    notesobj.push(Addtxt.value);
+    localStorage.setItem("notes",JSON.stringify(notesobj));
+
+    Addtxt.value="";
+    console.log(notesobj);
+
+    
+
+
+
+
+
+})
